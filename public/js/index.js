8f1cfe295ebc64e1cd37248c29a5ef155cc622dc
Brian Biggs <brianbiggs@Brians-MacBook-Pro.local>
2018-10-31 12:57:48 -0400

    Add form for message and show incomping message in browser

--- public/js/index.js
+++ public/js/index.js
@@ -1,26 +1,34 @@
-
-//    var x = 0;
 var socket = io();
+
 socket.on('connect', function () {
   console.log('Connected to server');
-  // socket.emit('createEmail', {
-  //   to: 'jen@example.com',
-  //   text: 'Hey. This is Andrew!'
-  // });
-  // socket.emit('newMessage', {
-  //   from: 'bkbiggs',
-  //   text: 'Yep, I am here.'
-  // });
 });
 
 socket.on('disconnect', function () {
   console.log('Disconnected from server');
 });
 
-// socket.on('newEmail', function(email) {
-//   console.log('New email', email);
-// });
-
 socket.on('newMessage', function(message) {
   console.log('newMessage', message);
+  var li = jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+
+  jQuery('#messages').append(li);
+});
+
+// socket.emit('createMessage', {
+//   from: 'Frank',
+//   text: 'hi'
+// }, function (data) {
+//   console.log('Got it', data);
+// });
+
+jQuery('#message-form').on('submit', function(e) {
+  e.preventDefault();
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  }, function() {
+
+  });
 });
